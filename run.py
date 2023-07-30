824479634bd7e4dce7445afb5c8d8f08266df59b
aimeeallen94 <>
2023-07-30 15:45:43 +0000

    getting average number of sandwiches to be made

--- run.py
+++ run.py
@@ -118,6 +118,21 @@ def get_last_5_enteries_sales():
         columns.append(column[-5:])
     return columns 
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print('Calculating stock data...\n')
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+print(new_stock_data)
+
 def main():
     """
     Runs all program functions
@@ -132,3 +147,4 @@ print('Welcome to Love Sandwiches Data Automation')
 #main()
 
 sales_columns = get_last_5_enteries_sales()
+calculate_stock_data(sales_columns)
